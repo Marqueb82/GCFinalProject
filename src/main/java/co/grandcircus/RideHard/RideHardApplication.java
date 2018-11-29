5c0c4e8d60ca1f9a2dcae1be7d51c9085673f346
Ed Carter <carteroffice@gmail.com>
2018-11-29 16:16:39 -0500

    initial commit ... nothing's broken, yet.

---
+++ src/main/java/co/grandcircus/RideHard/RideHardApplication.java
@@ -0,0 +1,12 @@
+package co.grandcircus.RideHard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RideHardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RideHardApplication.class, args);
+	}
+}
---
+++ src/test/java/co/grandcircus/RideHard/RideHardApplicationTests.java
@@ -0,0 +1,16 @@
+package co.grandcircus.RideHard;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RideHardApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
