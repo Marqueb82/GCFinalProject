a9943576a595901ce2ebcfc0cd3b6461b7f44fe1
Ed Carter <carteroffice@gmail.com>
2018-11-30 00:42:25 -0500

    basic examples and copy fodder

--- src/main/java/co/grandcircus/RideHard/RideController.java
+++ src/main/java/co/grandcircus/RideHard/RideController.java
@@ -1,7 +1,13 @@
 package co.grandcircus.RideHard;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -9,7 +15,86 @@ public class RideController {
 	
 	@RequestMapping("/")
 	public ModelAndView home() {
-		return new ModelAndView("index");
+		ModelAndView mv = new ModelAndView("index");
+		String aString = "I'm a String, about to be sent to a JSP!";
+		mv.addObject("JSP_data", aString);
+		return mv;
 	}
 
+	//None of these controller methods really matter at this point from here on. These are to serve as placeholders only to prove the API calls are working.
+	
+	//potential controller to demonstrate Ticket Master API call
+	@RequestMapping("/ticketmasterAPI")
+	public ModelAndView tmAPI(@RequestParam("somedata") String probably) {
+		ModelAndView mv = new ModelAndView("tmAPI");
+		
+		return mv;
+	}
+	
+	//ditto CoOrd API
+	@RequestMapping("/CoOrdAPI")
+	public ModelAndView coOrdAPI(@RequestParam("somedata") String probably) {
+		ModelAndView mv = new ModelAndView("coOrdAPI");
+		
+		return mv;
+	}
+	
+	//Same, Uber
+	@RequestMapping("/uberAPI")
+	public ModelAndView ubAPI(@RequestParam("somedata") String probably) {
+		ModelAndView mv = new ModelAndView("ubAPI");
+		
+		return mv;
+	}
+	
+	//etc. 
+	@RequestMapping("/lyftAPI")
+	public ModelAndView lyAPI(@RequestParam("somedata") String probably) {
+		ModelAndView mv = new ModelAndView("lyAPI");
+		
+		return mv;
+	}
+	
+	
+	@RequestMapping("/spotHeroAPI")
+	public ModelAndView shAPI(@RequestParam("somedata") String probably) {
+		ModelAndView mv = new ModelAndView("shAPI");
+		
+		return mv;
+	}
+	
+	
+	//I forget if we landed on actually using this or how, but seems good.
+	@RequestMapping("/mapsAPI")
+	public ModelAndView gmAPI(@RequestParam("somedata") String probably) {
+		ModelAndView mv = new ModelAndView("gmAPI");
+		
+		return mv;
+	}
+
+	//Demo of list display in JSP
+	@RequestMapping("/list")
+	public ModelAndView lists() {
+		ModelAndView mv = new ModelAndView("listexample");
+		List<Integer> numbersList = new ArrayList<Integer>();
+		numbersList.add(4);
+		numbersList.add(5);
+		numbersList.add(8);
+		numbersList.add(34);
+		numbersList.add(9);
+		numbersList.add(2);
+		mv.addObject("numbers", numbersList);
+		
+		Map<String, Integer> hm = new HashMap<>();
+		//Don't know what determines what order the HashMap displays in the JSP.
+		hm.put("What", 100);
+		hm.put("order", 99);
+		hm.put("are", 98);
+		hm.put("these", 999);
+		hm.put("in", 97);
+		mv.addObject("Dec6th", hm);
+				
+		return mv;
+	}
+	
 }
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,25 @@
+<%@ page
+	language="java"
+	contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"
+%>
+<%@ taglib
+	uri="http://java.sun.com/jsp/jstl/core"
+	prefix="c"
+%>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>This is an index.</title>
+</head>
+<body>
+	<h2>Come use our service. It's really rad.</h2>
+	<h4>This is a JSP that has had the tag we'll need to make JSPs do JSP stuff pasted at the top already. JSP files tell the browser what HTML to display.</h4>
+	
+	<h5>That string we sent from the controller will follow this sentence. ${ JSP_data } And one of us just learned that you can't include spaces in EL variables!</h5>
+	
+	
+</body>
+</html>
